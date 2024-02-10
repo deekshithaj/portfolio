3f08e30a669dfda2272717cc29916493284752b4
Deekshitha Juvvadi <deekshithajuvvadi@Deekshithas-MacBook-Pro.local>
2024-02-10 03:40:36 -0600

    updating changes-latest

--- src/components/nav.js
+++ src/components/nav.js
@@ -208,7 +208,7 @@ const Nav = ({ isHome }) => {
   const ResumeLink = (
     <a
       className="resume-button"
-      href="../DeekshithaJuvvadi_Resume_v.pdf"
+      href="DeekshithaJuvvadi_Resume_v.pdf"
       target="_blank"
       rel="noopener noreferrer">
       Resume
